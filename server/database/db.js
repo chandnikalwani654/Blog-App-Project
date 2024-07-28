64b5aa73d11a305435371236ba7db25135858920
githubchandni <113281125+githubchandni@users.noreply.github.com>
2024-07-28 22:54:48 +0530

    Add files via upload

---
+++ server/database/db.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+ const Connection = async (username,password) => {
+    const URL=`mongodb+srv://${username}:${password}@blog.0fqelnv.mongodb.net/?retryWrites=true&w=majority&appName=Blog`;
+    try{
+       await mongoose.connect(URL,{ useUnifiedTopology: true});
+       console.log('Database connected successfully');
+    }
+    catch(error){
+       console.log('Error while connecting with the database',error);
+    }
+    
+}
+export default Connection;
