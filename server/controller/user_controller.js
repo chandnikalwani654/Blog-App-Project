774b8272b7db7c70f6cafc1417adafea3304196a
githubchandni <113281125+githubchandni@users.noreply.github.com>
2024-07-31 23:11:44 +0530

    Add files via upload

--- server/controller/user_controller.js
+++ server/controller/user_controller.js
@@ -1,7 +1,16 @@
+import User from '../model/user.js';
 
 
 
+export const signupUser = async(req,res)=>{
+    try{
+        const user=req.body;
 
-export const signupUser=(req,res)=>{
+        const newUser=new User(user);
+        await newUser.save();
 
+        res.status(200).json({msg:'Signup successfull'})
+    } catch(error){
+          return res.status(500).json({msg:'Error while signup the user'}) 
+    }
 }
