3066fa8012748631641da8ee86e0b24fe39d3030
githubchandni <113281125+githubchandni@users.noreply.github.com>
2024-08-03 23:28:04 +0530

    Add files via upload

--- client/src/components/account/login.jsx
+++ client/src/components/account/login.jsx
@@ -75,8 +75,6 @@ const SignupButton=styled(Button)`
    box-shadow:0 2px 4px 0 rgb(0 0 0/20%);
 `
 
-
-
 const signupInitialValues={
     name:"",
     username: "",
--- client/src/service/api.js
+++ client/src/service/api.js
@@ -9,3 +9,51 @@ const axiosInstance=axios.create({
         "content-type":"application/json"
     }
 })
+
+axiosInstance.interceptors.request.use(
+    function (config){
+        return config;
+    },
+    function (error){
+        return Promise.reject(error);
+    }
+)
+
+axiosInstance.interceptors.response.use(
+    function (response){
+        //Stop Global Loader
+        return processResponse(response);
+    },
+    function (error){
+        //Stop Global Loader
+        return Promise.reject(processError(error));
+    }
+)
+
+
+/////////////////////////////
+//If success -> return {isSuccess:true , data:Object}
+//If fail -> return {isFailure:true , status:string , msg:string , code:int}
+/////////////////////////////
+
+const processResponse = (response) => {
+    if(response?.status===200){
+      return{ isSuccess:true , data : response.data }
+    } else {
+        return{ isFailure:true , status: response?.status , msg: response?.msg, code:response?.code }
+    }
+}
+
+const processError = (error) => {
+    if(error.response){
+        //Request made and server responded with a status other that falls out of the range 2.x.x(200)
+        
+    } else if(error.request){
+        //Request made but no response was received
+        
+    } else {
+        //Something happened in setting up request that triggers an error 
+
+    }
+}
+
